94691834ee3378561bffd5cbf500ab2e20619343
djaydevs <80023967+djaydevs@users.noreply.github.com>
2022-12-04 22:32:41 +0800

    update added navigation
    changes includes:
    dashboardactivity
    drawerbaseactivity
    contentlayout
    drawerheader & menu
    deleted night theme

---
+++ app/src/main/java/com/example/fillupautomationapp/DashboardActivity.java
@@ -0,0 +1,21 @@
+package com.example.fillupautomationapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.fillupautomationapp.databinding.ActivityDashboardBinding;
+
+public class DashboardActivity extends DrawerBaseActivity {
+
+    ActivityDashboardBinding activityDashboardBinding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+
+        activityDashboardBinding = ActivityDashboardBinding.inflate(getLayoutInflater());
+        setContentView(activityDashboardBinding.getRoot());
+    }
+}
